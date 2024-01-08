27232d072c11920ba92561bcb86cf50be194e48d
RudychikFedorV <rudychik.fedor@gmail.com>
2024-01-08 21:26:15 +0200

    commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="zone-header">
+      <Link to={"/"}>1</Link>
+    </div>
+  );
+};
+
+export default Header;
