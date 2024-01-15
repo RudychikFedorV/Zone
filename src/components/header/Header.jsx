1926db4c90e939993e87c47811e9fe5f0edb9507
RudychikFedorV <rudychik.fedor@gmail.com>
2024-01-15 19:46:42 +0200

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,14 +6,16 @@ const Header = () => {
   return (
     <div className="zone-header">
         <HeaderLogo />
-      <div className="zone-header__navigation">
+      <div className="zone-header__navigation" id="headerNav">
         <Link to={"/"} className="color">Home</Link>
         <Link to={"/"} className="color">About Us</Link>
         <Link to={"/"} className="color">Service</Link>
         <Link to={"/"} className="color">Pricing</Link>
         <Link to={"/"} className="color">Blog</Link>
         <Link to={"/"} className="btn color">Contact Us</Link>
+        <span className="material-icons icon-close" onclick="navToggle()">close</span>
       </div>
+      <span className="material-icons icon-menu" onclick="navToggle()">menu</span>
     </div>
   );
 };
